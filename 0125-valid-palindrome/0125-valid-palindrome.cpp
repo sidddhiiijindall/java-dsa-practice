ffd83942b86674643e4e251c4149437bc576b9d9
sidddhiiijindall <siddhijindal0407@gmail.com>
2026-04-25 20:54:18 +0530

    Time: 0 ms (100%), Space: 10 MB (53.27%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int i =0;
-        string st ;
-        for(int i =0;i<s.length();i++){
-            if(s[i]>=97 && s[i]<= 122 || s[i]>='0' && s[i]<='9'){
-                st.push_back(s[i]);
-            }else if(s[i]>= 65 && s[i]<= 90) st.push_back(s[i] + 32);
-        }
-         int n= st.length()-1;
+        int i =0, n= s.length()-1;
         while(i<n){
-            
-            if(st[i]==st[n]){
-                i++;
-                n--;
-            }
-           
-            else{
+            if(!isalnum(s[i])){i++;}
+            else if(!isalnum(s[n])){n--;}
+            else if(tolower(s[i])!=tolower(s[n])){
                 return false;
             }
+           else{i++,n--;}
+           
         }
         return true;
     }
