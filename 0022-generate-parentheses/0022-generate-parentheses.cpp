a31efe0bd4189bd473e4d00632a9ca432b6d2d32
sidddhiiijindall <siddhijindal0407@gmail.com>
2026-07-02 10:31:34 +0530

    Time: 3 ms (70.21%), Space: 13.1 MB (83.49%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+ void fn(int n,string& s,int& o, int& c, vector<string>& ans ){
+    if(s.length() == n*2 ){
+        if(o==c) ans.push_back(s);
+          return ;
+    }
+    if( s.length()==0 || o<n){
+        s.push_back('(');
+        o++;
+        fn(n,s, o,c,ans);
+        s.pop_back();
+        o--;
+        if(o>c){
+         s.push_back(')');
+        c++;
+         fn(n,s, o,c, ans);
+         s.pop_back();
+         c--;
+        }
+    }
+    else{
+        s.push_back(')');
+        c++;
+        fn(n,s,o,c,ans);
+        s.pop_back();
+        c--;
+    }
+ }
+    vector<string> generateParenthesis(int n) {
+        int  o=0,c=0;
+        string s="";
+        vector<string> ans;
+        fn(n,s ,o,c ,ans);
+        return ans;
+    }
+};
