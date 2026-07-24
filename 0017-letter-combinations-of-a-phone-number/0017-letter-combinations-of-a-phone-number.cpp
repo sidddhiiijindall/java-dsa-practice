7a5cd2277114f4b319b632361494e0f891df4a17
sidddhiiijindall <siddhijindal0407@gmail.com>
2026-07-24 19:57:09 +0530

    Time: 0 ms (100%), Space: 9.3 MB (62.79%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-  void fn (string digits, vector<string>& ans, int i ,vector<string>& check, string& t){
-    if(t.length()== digits.length()){
+ void fn(string digits , string& t , vector<string>& ans , int i , vector<string>& alph){
+    if(i==digits.size()){
         ans.push_back(t);
-        return ;
+        return;
     }
-    int n = digits[i]-'0';
-    string a= check[n-2];
+   int ind = digits[i]-'0';
+    string a= alph[ind-2];
     for(int j =0;j< a.length();j++ ){
         t.push_back(a[j]);
-        fn(digits , ans , i+1 ,check , t);
+        fn(digits , t , ans , i+1, alph);
         t.pop_back();
     }
-    
-  }
+    return;
+ }
     vector<string> letterCombinations(string digits) {
-       vector<string> check;
-       check.push_back("abc");
-        check.push_back("def");
-         check.push_back("ghi");
-          check.push_back("jkl");
-           check.push_back("mno");
-            check.push_back("pqrs");
-             check.push_back("tuv");
-              check.push_back("wxyz");
-              string t="";
-              vector<string> ans; 
-              fn(digits, ans , 0, check , t);
-              return ans;
+        vector<string> alph;
+        alph.push_back("abc");
+        alph.push_back("def");
+        alph.push_back("ghi");
+        alph.push_back("jkl");
+        alph.push_back("mno");
+        alph.push_back("pqrs");
+        alph.push_back("tuv");
+        alph.push_back("wxyz");
+        string t ="";
+        vector<string> ans ;
+        fn(digits , t , ans , 0 , alph);
+        return ans ;
     }
 };
