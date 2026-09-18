bec6eff919bb7d76aeed81fb3b5c27e18cdee39d
sidddhiiijindall <siddhijindal0407@gmail.com>
2026-09-18 09:21:05 +0530

    Time: 0 ms (100%), Space: 7.9 MB (71.89%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -8,8 +8,8 @@ public:
         while(l<=h){
             int mid = l+(h-l)/2;
             if(isBadVersion(mid) == false)l=mid+1;
-            else{ans=mid , h=mid-1;}
+            else{ h=mid-1;}
         }
-        return ans;
+        return l;
     }
 };
